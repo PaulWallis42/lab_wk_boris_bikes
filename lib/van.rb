1fddcd5ebf2312d3ff340110d65855ecbf93df5f
paulwallis42 <paulwallis42@gmail.com>
2015-04-20 19:40:31 +0100

    fully working van class not refactored

--- lib/van.rb
+++ lib/van.rb
@@ -1,5 +1,6 @@
 require_relative 'bike'
 require_relative 'docking_station'
+require_relative 'garage'
 
 class Van
   attr_reader :bikes
@@ -11,6 +12,13 @@ class Van
 
   def add_bike(bike, from)
     fail 'Bike is not broken' if bike.broken? == false
+    fail 'Van is full' if full?
+    @bikes << bike
+    from.bikes.delete(bike)
+  end
+
+  def add_fixed_bike(bike, from)
+    fail 'Bike is still broken' if bike.broken? == true
     fail 'Van is full' if full?
     @bikes << bike
     from.bikes.delete(bike)
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -13,11 +13,11 @@ describe Van do
     dock_station = double :docking_station, bikes: [:broken_bike, :unbroken_bike]
     expect { subject.add_bike unbroken_bike, dock_station }.to raise_error 'Bike is not broken'
   end
-  it 'can take a broken bike' do
+  it 'can take a broken bike from the docking station' do
     subject.capacity = 10
     broken_bike = double :bike, broken?: true
     dock_station = double :docking_station, bikes: [:broken_bike, :unbroken_bike]
-    subject.add_bike broken_bike, dock_station
+    subject.add_bike(broken_bike, dock_station)
     expect(subject.bikes).to eq([broken_bike])
   end
   it 'removes the taken bike from docking station' do
@@ -27,11 +27,38 @@ describe Van do
     subject.add_bike(broken_bike, dock_station)
     expect(dock_station.bikes).to eq([:unbroken_bike])
   end
-  it 'raises an error when full' do
+  it 'raises an error when full when collection bike from docking station' do
     subject.capacity = 10
-    unbroken_bike = double :bike, broken?: true
+    broken_bike = double :bike, broken?: true
     dock_station = double :docking_station, bikes: [:broken_bike, :unbroken_bike]
-    10.times { subject.add_bike unbroken_bike, dock_station }
-    expect { subject.add_bike unbroken_bike, dock_station }.to raise_error 'Van is full'
+    10.times { subject.add_bike broken_bike, dock_station }
+    expect { subject.add_bike broken_bike, dock_station }.to raise_error 'Van is full'
+  end
+  it 'can get bike from the garage' do
+    subject.capacity = 10
+    fixed_bike = double :bike, broken?: false
+    garage = double :garage, bikes: [fixed_bike, :broken_bike]
+    subject.add_fixed_bike(fixed_bike, garage)
+    expect(subject.bikes).to eq([fixed_bike])
+  end
+  it 'removes the bike from the garage' do
+    subject.capacity = 10
+    fixed_bike = double :bike, broken?: false
+    garage = double :garage, bikes: [fixed_bike, :broken_bike]
+    subject.add_fixed_bike(fixed_bike, garage)
+    expect(garage.bikes).to eq([:broken_bike])
+  end
+  it 'raises an error if full when collection bike from garage' do
+    subject.capacity = 10
+    unbroken_bike = double :bike, broken?: false
+    garage = double :garage, bikes: [:broken_bike, :unbroken_bike]
+    10.times { subject.add_fixed_bike(unbroken_bike, garage) }
+    expect { subject.add_fixed_bike(unbroken_bike, garage) }.to raise_error 'Van is full'
+  end
+  it 'can not take an unbroken bike' do
+    subject.capacity = 10
+    broken_bike = double :bike, broken?: true
+    garage = double :garage, bikes: [:broken_bike, :unbroken_bike]
+    expect { subject.add_fixed_bike(broken_bike, garage) }.to raise_error 'Bike is still broken'
   end
 end
